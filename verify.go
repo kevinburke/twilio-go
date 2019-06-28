cc1ad4141e040e0563cd44e5730cb0ebe6a5cb19
Elijah Oyekunle <eloyekunle@gmail.com>
2019-06-28 09:26:41 +0100

    add docstrings to methods

--- verify.go
+++ verify.go
@@ -43,18 +43,24 @@ type CheckPhoneNumber struct {
 	DateUpdated TwilioTime `json:"date_updated"`
 }
 
+// Create calls the Verify API to start a new verification.
+// https://www.twilio.com/docs/verify/api-beta/verification-beta#start-new-verification
 func (v *VerifyPhoneNumberService) Create(ctx context.Context, verifyServiceID string, data url.Values) (*VerifyPhoneNumber, error) {
 	verify := new(VerifyPhoneNumber)
 	err := v.client.CreateResource(ctx, servicesPathPart+"/"+verifyServiceID+"/"+verificationsPathPart, data, verify)
 	return verify, err
 }
 
+// Get calls the Verify API to retrieve information about a verification.
+// https://www.twilio.com/docs/verify/api-beta/verification-beta#fetch-a-verification-1
 func (v *VerifyPhoneNumberService) Get(ctx context.Context, verifyServiceID string, sid string) (*VerifyPhoneNumber, error) {
 	verify := new(VerifyPhoneNumber)
 	err := v.client.GetResource(ctx, servicesPathPart+"/"+verifyServiceID+"/"+verificationsPathPart, sid, verify)
 	return verify, err
 }
 
+// Check calls the Verify API to check if a user-provided token is correct.
+// https://www.twilio.com/docs/verify/api-beta/verification-check-beta#check-a-verification-1
 func (v *VerifyPhoneNumberService) Check(ctx context.Context, verifyServiceID string, data url.Values) (*CheckPhoneNumber, error) {
 	check := new(CheckPhoneNumber)
 	err := v.client.CreateResource(ctx, servicesPathPart+"/"+verifyServiceID+"/"+verificationCheckPart, data, check)
