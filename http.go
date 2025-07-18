5fe7adc01f29224740d1f91b1af5e0e74ee4191a
Kevin Burke <kevin@burke.dev>
2025-07-18 11:27:27 -0700

    2.9.0

--- http.go
+++ http.go
@@ -16,7 +16,7 @@ import (
 )
 
 // The twilio-go version. Run "make release" to bump this number.
-const Version = "2.8.0"
+const Version = "2.9.0"
 const userAgent = "twilio-go/" + Version
 
 // The base URL serving the API. Override this for testing.
